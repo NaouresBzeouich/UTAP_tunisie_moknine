1080acbdb1e4339e238dcea8380b06c52be8f166
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-08-16 11:54:46 +0100

    viewPage

--- ApplicationCode/PageDAccueil.py
+++ ApplicationCode/PageDAccueil.py
@@ -1,30 +1,33 @@
 import tkinter as tk
+from tkinter import ttk
+
 def HomePage():
     # Create the main window
     root = tk.Tk()
     # Adding a title to the main window
     root.title("UTAO tunisie moknine ")
     # Setting dimensions (width x height)
-    root.geometry("700x600")
-    #creating home page frame
-    home=tk.Frame(root)
-    #creating the command to the buttoms
+    root.geometry("800x600")
+    # creating home page frame
+    home = ttk.Frame(root)
+
+    # creating the command to the buttoms
     def AddAFarmer():
-        #to make sure that everything is okkey
+        # to make sure that everything is okkey
         print("Button to add a subscriber is clicked !")
         # creating the form page
-        form = tk.Frame(root)
+        form = ttk.Frame(root)
         # hiding the home page which is the current page
         home.pack_forget()
         # calling the function FormPage from the file formPage
         import formPage as fp
-        fp.FormPage(form,root)
+        fp.Form_Page(form, root, home)
 
     def ViewSubscriberList():
         # to make sure that everything is okkey
         print("Button to view subscriber list is clicked !")
         # creating the subscriber list page
-        view = tk.Frame(root)
+        view = ttk.Frame(root)
         # hiding the home page which is the current page
         home.pack_forget()
         # calling the function ViewSubscriberPage from the file viewSubscriberPage
@@ -32,15 +35,13 @@ def HomePage():
         vp.SubscriberPage(view)
 
     # Create buttons
-    AddingInscriptionButton = tk.Button(home, text="إضافة مشترك جديد ", command=AddAFarmer)
-    ViewSubscribersButton = tk.Button(home, text="عرض قائمة المشتركين", command=ViewSubscriberList)
+    AddingInscriptionButton = ttk.Button(home, text="إضافة مشترك جديد ", command=AddAFarmer)
+    ViewSubscribersButton = ttk.Button(home, text="عرض قائمة المشتركين", command=ViewSubscriberList)
 
     # Place buttons in the home page
     AddingInscriptionButton.pack(pady=20)
     ViewSubscribersButton.pack()
-    #showing the home page
+    # showing the home page
     home.pack()
     # Start the GUI event loop
     root.mainloop()
-
-
--- ApplicationCode/viewSubscribrPage.py
+++ ApplicationCode/viewSubscribrPage.py
@@ -1,3 +1,49 @@
 import tkinter as tk
+from tkinter import ttk
+
 def SubscriberPage(view):
-    view.pack()
+    def search():
+        excel_data = read_excel_data("subscriberlist.xlsx")
+        # to show data
+        for i, row_data in enumerate(excel_data):
+            for j, cell_value in enumerate(row_data):
+                label = tk.Label(excel_frame, text=str(cell_value))
+                label.grid(row=i + 10, column=j, padx=10, pady=10)
+
+    view.pack(fill="both", expand=True)
+
+    canvas = tk.Canvas(view)
+    canvas.pack(side="left", fill="both", expand=True)
+    # to create the scrollbar in the left
+    scrollbar = ttk.Scrollbar(view, command=canvas.yview)
+    scrollbar.pack(side="right", fill="y")
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    excel_frame = ttk.Frame(canvas)
+    canvas.create_window((0, 0), window=excel_frame, anchor="nw")
+    # create a place to do searching
+    labels = [": اسم ", ": اللقب ", " : رقم بطاقة الهوية الوطنية ", ": رقم الهاتف ", " : المنطقة ", " : تخصص"]
+    entries = [tk.Entry(excel_frame) for _ in range(6)]
+    # Place labels and entry fields in the frame
+    for i, label in enumerate(labels):
+        ttk.Label(excel_frame, text=label).grid(row=i , column=2, padx=20, pady=5)
+        entries[i].grid(row=i, column=1, padx=10, pady=5)
+    # creating search button
+    submit_button = ttk.Button(excel_frame, text="بحث عن المشتركين ", command=search)
+    submit_button.grid(row=8, columnspan=3, padx=10, pady=10)
+    # to show data first row
+    for i, label in enumerate(labels):
+        ttk.Label(excel_frame, text=label).grid(row=9, column=i, padx=20, pady=5)
+    excel_frame.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
+
+def read_excel_data(file_path):
+    import openpyxl
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+
+    data = []
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        data.append(row)
+
+    return data
+
