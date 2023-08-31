39cfa3c1e6bc2330f1f718d47e799a2c3763c068
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-08-31 21:30:53 +0100

    adding checkbtn for every row in deletion page front-end

--- ApplicationCode/DeletionListPage.py
+++ ApplicationCode/DeletionListPage.py
@@ -18,26 +18,41 @@ def DeletionList(deleteList, home, rows_to_delete, row_number):
     def return_to_home():
         deleteList.pack_forget()
         home.pack()
+
     # function to delete subscribers
     def deletion():
         print("okkey")
         return_to_home()
+
+    # function when checkbtn is clicked
+    def toggle_tik(i):
+        print(table[i])
+
     if rows_to_delete:
         # text for make sure the deletion
         ttk.Label(List, text="  هل  أنت  متأكد  من  حذف  هؤلاء  المشتركين   ", font=("Helvetica", 60), anchor="center",
                   foreground="red", background="white").grid(row=1, column=1, columnspan=8, padx=10, pady=10)
         # table contain selected subscriber
-        labels = ["\t",": اسم ", ": اللقب ", " : رقم بطاقة الهوية الوطنية ", ": رقم الهاتف ", " : المنطقة ",
-                  " :النقابات القطاعية","\t"]
+        labels = ["\t", ": اسم ", ": اللقب ", " : رقم بطاقة الهوية الوطنية ", ": رقم الهاتف ", " : المنطقة ",
+                  " :النقابات القطاعية", "\t"]
         # to show data first row
         ttk.Label(List, text="\t\t\t", padding=8, font=("Helvetica", 20)).grid(row=2, column=0, pady=20)
         for i, label in enumerate(labels):
             ttk.Label(List, text=label, relief="solid", borderwidth=1, padding=8, anchor="e",
                       font=("Helvetica", 20)).grid(row=2, column=i + 1, pady=20, sticky="nsew")
+
+        # Create  Checkbutton variables
+        tik_var = [tk.IntVar() for _ in range(row_number)]
+        table = [i for i in range(row_number)]
+        print(table)
+
         # to show deletion data
         grid_cell = []
         for i, row_data in enumerate(rows_to_delete):
             row_cell = []
+            # Create a Checkbutton widget
+            tik_button = tk.Checkbutton(List, variable=tik_var[i], command=lambda: toggle_tik(i))
+            tik_button.grid(row=i+3, column= 1, padx=10, pady=10)
             for j, cell_value in enumerate(row_data):
                 if j == 6:
                     if cell_value is None:
